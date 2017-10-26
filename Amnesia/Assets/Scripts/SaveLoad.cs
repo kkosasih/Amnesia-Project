253698a3c31374d2dc98e8e964dffedcf6787dae
Tyler Gemora <tgemora@uci.edu>
2017-10-26 14:17:38 -0700

    Added script files SaveLoad, PlayerController, and GameController. Currently, you can save player location with ',' and load it with '.' since that's all I have to work with.

---
+++ Amnesia/Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoad {
+    public static GameObject player = GameObject.FindWithTag("Player");
+
+    // Saves the needed game info
+    public static void Save ()
+    {
+        if (player != null)
+        {
+            PlayerPrefs.SetFloat("PlayerPosX", player.transform.position.x);
+            PlayerPrefs.SetFloat("PlayerPosY", player.transform.position.y);
+            Debug.Log("Player location saved");
+        }
+    }
+
+    // Loads the needed game info
+    public static void Load ()
+    {
+        if (player != null)
+        {
+            player.transform.position = new Vector3(PlayerPrefs.GetFloat("PlayerPosX", player.transform.position.x), PlayerPrefs.GetFloat("PlayerPosY", player.transform.position.y), 0);
+            Debug.Log("Player location loaded");
+        }
+    }
+}
