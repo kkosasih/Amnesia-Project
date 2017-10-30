68cc527241929d2a432a26cfc02e02528753644c
Wilfredo Paraiso <wilfredoiparaisojr@gmail.com>
2017-10-29 20:55:56 -0700

    Item list and Partial Inventory

---
+++ Amnesia/Assets/Scripts/Item.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item : MonoBehaviour
+{
+    public string itemName;
+    public int itemId;
+    public string itemDesc;
+    public Texture2D itemIcon;
+    public ItemType itemtype;
+
+    public enum ItemType
+    {
+        Tool,
+        Consumable,
+        Quest
+    }
+
+    public Item(string name, int id, string desc, ItemType type)
+    {
+        itemName = name;
+        itemId = id;
+        itemDesc = desc;
+        itemIcon = Resources.Load<Texture2D>("Item Icons/" + name);
+        itemtype = type;
+    }
+
+    public Item()
+    {
+
+    }
+}
+
