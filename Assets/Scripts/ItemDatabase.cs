2048afda0a92207e57ff5e8373dd646ba096012d
Tyler Gemora <tgemora@uci.edu>
2017-11-02 10:46:56 -0700

    Changed the folder that the project was contained in, made objects retain properties between scenes, and added save/loads to inventory slots.

---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour {
+    public List<Item> items = new List<Item>();
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        //Got bored started making random items
+        items.Add(new Item("vest", 0, 2, "A cloth vest made of cloth", 1, Item.ItemType.Tunic));
+        items.Add(new Item("Short Sword", 1, 4, "A teeny tiny sword named sword", 1, Item.ItemType.Equipable));
+        items.Add(new Item("Apple", 2, 2, "Unusually red fruit \"It's red like your Health bar\"- Unknown Man #1", 5, Item.ItemType.Consumable));
+        items.Add(new Item("Garbage", 3, 999, "It's garbage \"One man's trash...\" - Older Unknown Man", 999, Item.ItemType.Quest));
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tile : MonoBehaviour {
+    public TileType type;                            
+    public Vector2 position;                    
+    public bool hasAttack = false;              
+    private SpriteRenderer _spriteRenderer;     
+
+    public enum TileType
+    {
+        Ground,
+        Wall,
+        Entrance
+    }
+
+    void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        UpdatePosition(position);
+        UpdateColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Change position of the tile
+    public void UpdatePosition (Vector2 positionP)
+    {
+        position = positionP;
+        transform.localPosition = position;
+    }
+
+    // Switch the tile's attack state
+    public IEnumerator GiveAttack (float time)
+    {
+        hasAttack = true;
+        _spriteRenderer.color = new Color(1, 0, 0);
+        yield return new WaitForSeconds(time);
+        hasAttack = false;
+        UpdateColor();
+    }
+
+    // Update the color of the tile
+    private void UpdateColor ()
+    {
+        switch (type)
+        {
+            case TileType.Ground:
+                _spriteRenderer.color = new Color(0, 1, 0);
+                break;
+            case TileType.Wall:
+                _spriteRenderer.color = new Color(1, 1, 0);
+                break;
+            case TileType.Entrance:
+                _spriteRenderer.color = new Color(0, 0, 1);
+                break;
+        }
+    }
+}
