fd741f16740326d82bf419bee097c257e173636b
tubigervie <etubig@uci.edu>
2017-11-05 14:30:51 -0800

    Monster Stats (unfinished)

---
+++ Amnesia/Assets/MonsterStats.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonsterStats : MonoBehaviour
+{
+    public int enemyHealth;
+    public int maxEnemyHealth;
+    private Slider enemyHealthSlider;
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    void ApplyDamage(int damage)
+
+    {
+        enemyHealth -= damage;
+        if (enemyHealth <= 0)
+        {
+            EnemyDeath();
+        }
+    }
+
+    void EnemyDeath()
+    {
+        Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+        enemyHealthSlider = GameObject.Find("HealthSlider").GetComponent<Slider>();
+
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    void ChangeHealth(int newenemyHealth)
+
+    {
+        enemyHealth = newenemyHealth;
+
+        enemyHealthSlider.value = (float)enemyHealth / maxEnemyHealth * 100;
+    }
+
+}
