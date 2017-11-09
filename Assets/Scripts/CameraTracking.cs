389a4ae339d1e9338a730d06d1c5c019fcc7346e
Tyler Gemora <tgemora@uci.edu>
2017-11-09 15:00:07 -0800

    Added basic camera tracking

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    // Moves the camera to the player
+    public void UpdatePos (Vector3 newPos)
+    {
+        transform.position = newPos;
+    }
+}
