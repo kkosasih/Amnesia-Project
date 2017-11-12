cc500ddf96ed5f54c670655e09a67bcbf53e2a41
Wilfredo Paraiso <wilfredoiparaisojr@gmail.com>
2017-11-11 18:08:10 -0800

    Pushing progress for item drops
    -Need to change inventory code to automatically change for loop size
    -Need to have items take into account stackable items
    -Need to figure out why script for prefabs isn't detected
    -ETC.

--- Assets/Scripts/ItemDatabase.cs
+++ Assets/Scripts/ItemDatabase.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ItemDatabase : MonoBehaviour {
     public List<Item> items = new List<Item>();
+    public string tempname;
+    public int var1;
+    public int var2;
+    public int var3;
 
     void Start()
     {
@@ -16,5 +20,63 @@ public class ItemDatabase : MonoBehaviour {
         items.Add(new Item("Neke Air Jordan's", 5, 1, "\"It's in the game\" - Spokesperson", 900, 1, Item.ItemType.Boots));
         items.Add(new Item("Oven Mitt", 6, 7, "It's one oven mitt \"Your gloveless hand feels cold\"", 50, 2, Item.ItemType.Gautlets));
         items.Add(new Item("Stained Pot", 7, 255, "Label on side: \"Now made with stainless steel!!\"", 20, 1, Item.ItemType.Helmet));
+    }
+
+    public string randomdrop(string tempname)
+    {
+        var1 = Random.Range(1, 100);
+        var2 = Random.Range(1, 10);
+        var3 = Random.Range(1, 10);
+
+        if (var1 <= 5)                          //5%
+        {
+            if(tempname == "slime")
+            {
+                tempname = "vest";
+            }
+        }
+        else if ((var1 > 5) && (var1 <= 15))    //10%
+        {
+            if (tempname == "slime")
+            {
+                tempname = "Short Sword";
+            }
+        }
+        else if ((var1 > 15) && (var1 <= 35))   //20%
+        {
+            if (tempname == "slime")
+            {
+                tempname = "Apple";
+            }
+        }
+        else if ((var1 > 35) && (var1 <= 65))   //30%
+        {
+            if (tempname == "slime")
+            {
+                tempname = "Apple";
+            }
+        }
+        else if ((var1 > 65) && (var1 <= 85))   //20%
+        {
+            if (tempname == "slime")
+            {
+                tempname = "Apple";
+            }
+        }
+        else if ((var1 > 85) && (var1 <= 95))   //10%
+        {
+            if (tempname == "slime")
+            {
+                tempname = "Apple";
+            }
+        }   
+        else                                    //5%
+        {
+            if (tempname == "slime")
+            {
+                tempname = "Apple";
+            }
+        }
+        return tempname;
     }
 }
---
+++ Assets/Scripts/ItemDropScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDropScript : MonoBehaviour {
+    public int SlimeAmount;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int j = 0; j < 25; j++)// Remember to increase depending on size of database
+        {
+            if (name == database.items[j].itemName)
+            {
+                inventory[i] = database.items[j];
+                break;
+            }
+        }
+    }
+}
