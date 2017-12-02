ea1c00337e66a9b8c9b543d5a7606b0150088016
Wilfredo Paraiso <wilfredoiparaisojr@gmail.com>
2017-12-01 20:36:36 -0800

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -14,9 +14,8 @@ public class Item
     public int itemStackAmount;
     public int itemValue;               //The monatery value of the item
     public Texture2D itemIcon;          //How it looks
-    public ItemType itemtype;           //What type of item is it
 
-    public enum ItemType
+    /*public enum ItemType
     {
         Equipable,                      //Tools
         Tunic,                          //Chest Wearable
@@ -26,26 +25,23 @@ public class Item
         Helmet,                         //...REASONS....
         Consumable,                     //..............
         Quest                           //Non-Discardable, Sellable, or Equipable items
-    }
+    }*/
 
-    public Item(string name, int id, int output, string desc, int value, int stack, int amount, ItemType type)
+    public Item(string name, int id, int output, string desc, int value, int stack, int amount)
     {
         itemName = name;
         itemId = id;
         itemOutput = output;
+
         itemDesc = desc;
         itemStack = stack;
         itemStackAmount = stack;
         itemValue = value;
         itemIcon = Resources.Load<Texture2D>("Item Icons/" + name);
-        itemtype = type;
     }
 
     public Item()
     {
 
     }
-
-
 }
-
