8dc4556a16a82fa11a2138edfa3ff07e8166dd15
Tyler Gemora <tgemora@uci.edu>
2017-12-29 21:30:58 -0800

    Changed size of interaction indicator.
    Added Walking Animation Prototype L/R View, and Back View

---
+++ Assets/Scripts/Story/ChangeCam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCam : DialoguePart
+{
+    private float time3;
+    private float start;
+
+    // Change the variables based on a string
+    public override void ChangeSettings(string data)
+    {
+        base.ChangeSettings(data);
+        string[] parameters = data.Split('|');
+        time3 = float.Parse(parameters[2]);
+        string[] vectorPoints = parameters[3].Split(',');
+    }
+
+    // Wait for time1, move the camera for time3 seconds, then wait for time2
+    public override IEnumerator PerformPart()
+    {
+        isRunning = true;
+        yield return new WaitForSeconds(time1);
+        yield return new WaitForSeconds(time2);
+        isRunning = false;
+    }
+}
