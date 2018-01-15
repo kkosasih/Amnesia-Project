0a0cce8ff8d835ba7cbfa719d6387c8f5a0f6648
Wilfredo Paraiso <wilfredoiparaisojr@gmail.com>
2018-01-15 14:44:14 -0800

    Auto stash before merge of "master" and "origin/master"
    Trying to not lose progress.

--- Assets/Scripts/zOther/QuestTracking.cs
+++ Assets/Scripts/zOther/QuestTracking.cs
@@ -8,56 +8,104 @@ public class QuestTracking : MonoBehaviour
     public List<QuestTypes> availablequests;
     public List<QuestTypes> mainList;
     public List<QuestTypes> questList;
+    public List<QuestTypes> fquestList;
     public int[,] character;
     public GameObject Inventory;
     public GameObject Speaker;
     public GameObject FinUI;
+    public CanvasGroup FinUI2;
+    public bool finuiup = false;
     public Text Title;
     public Text speaker;
-    public int questinc = 0;
+    public Text stfin;
+    public string questname;
+    public int questinc = 0, questinc2 = 0;
+    public float num = 0;
 
     void Start()
     {
         mainList = new List<QuestTypes>();
         questList = new List<QuestTypes>();
+        fquestList = new List<QuestTypes>();
         character = new int[10, 2];
-        mainList.Add(QuestDatabase.MainQuest1());
+        
+        //mainList.Add(QuestDatabase.MainQuest1());
         mainList.Add(QuestDatabase.MainQuest2());
         //questList.Add(QuestDatabase.MainQuest1());
+        addingquests("GeneralQuest1");
+
+        FinUI2 = FinUI.GetComponent<CanvasGroup>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        mainList[questinc].updatedprog();
-        if (mainList[questinc].finished == true)
-       {
-            FinishQuest(questinc);// May want to add something that waits until the ui has disappeared then brings it back
-        }        
-
-        /*for (int z = 0; z < questList.Count; z++)
+        if(speaker.text != "Name")
         {
-            if (questList[z].finished == false)
+            speakobj();
+        }
+
+        //For getting the started quest menu
+        if((questinc2 != questinc) && (finuiup == false))
+        {
+            stfin.text = "Quest Start";
+            FinUI2.alpha = 0;
+            FinUI.SetActive(true);
+            finuiup = true;
+            Title.text = mainList[questinc].questname;
+            StartCoroutine(Fade(FinUI2));
+            questinc2 += 1;
+        }
+
+        for (int z = 0; z < questList.Count; z++)
+        {
+            if (finuiup == false && questList[z] != null && questList[z].mainquest != true)
             {
-                questList[z].updatedprog();
-                if (questList[z].finished == true)
-                {
-                    FinishQuest(z);
-                }
+                stfin.text = "Quest Start";
+                FinUI2.alpha = 0;
+                finuiup = true;
+                FinUI.SetActive(true);
+                Title.text = questList[z].questname;
+                questList[z].mainquest = true;
+                StartCoroutine(Fade(FinUI2));
             }
-        }*/
+        }
+
+        //For Keeping track of the progress of the menu
+        mainList[questinc].updatedprog();
+        if (mainList[questinc].finished == true && finuiup == false)
+        {
+            finuiup = true;
+            FinishQuest(questinc);
+        }
+
+        //Make sure there isn't any big delay for any of the processes...
+
+        for (int z = 0; z < questList.Count; z++)
+        {
+            questList[z].updatedprog();
+            if (questList[z].finished == true && finuiup == false && questList[z] != null)
+            {
+                finuiup = true;
+                fquestList.Add(questList[z]);
+                FinishQuest(z);
+                questList[z] = null;
+            }
+        }
     }
 
     public void FinishQuest(int x)
     {
-        FinUI.SetActive(true);
-        if (mainList[x].mainquest == true)
+        FinUI2.alpha = 0;       
+        stfin.text = "Finished!!!";
+        if (mainList[x].finished == true)
         {
             Title.text = mainList[x].questname;
             for (int y = 0; y < mainList[x].questListloot.Count; y++)
             {
                 Inventory.GetComponent<Inventory>().AddItem(mainList[x].questListloot[y]);
             }
+            fquestList.Add(mainList[x]);
             character[mainList[x].character,0] += 1;
             character[mainList[x].character, mainList[x].characterprog] += 1;
             questinc += 1;
@@ -72,6 +120,8 @@ public class QuestTracking : MonoBehaviour
             character[questList[x].character, 0] += 1;
             character[questList[x].character, questList[x].characterprog] += 1;
         }
+        FinUI.SetActive(true);
+        StartCoroutine(Fade(FinUI2));
     }
 
     public void questobj(string target)
@@ -88,7 +138,42 @@ public class QuestTracking : MonoBehaviour
 
     public void speakobj()
     {
-        // Something checking you wanted to turn in the quest (put here)
+        // Something checking who you wanted to turn in the quest to (put here)
         questobj(speaker.text);
+    }
+
+    public IEnumerator Fade(CanvasGroup ui)
+    {
+        while (true)
+        {
+            ui.alpha += 0.05f;
+
+            if (ui.alpha >= 1)
+            {
+                break;
+            }
+
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        yield return new WaitForSeconds(2.0f);
+        while(true)
+        {
+            ui.alpha -= 0.05f;
+
+            if(ui.alpha <= 0)
+            {
+                break;
+            }
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        FinUI.SetActive(false);
+        finuiup = false;
+    }
+
+    public void addingquests(string qname)
+    {
+        questList.Add(QuestDatabase.Fix(qname));
     }
 }
--- Assets/Scripts/zOther/QuestTypes.cs
+++ Assets/Scripts/zOther/QuestTypes.cs
@@ -13,7 +13,7 @@ public class QuestTypes
     public int characterprog = 1; // If 1 then increases character path a else 2 for path b
     public string description;
     public string objective;
-    public bool mainquest;
+    public bool mainquest;  //Need to change name to something like startuicalled
     public bool finished;
 
     public void updatedprog()
