b29423bd8504bc844808f3b877a1bd903aaab191
Tyler Gemora <tgemora@uci.edu>
2018-01-25 16:59:41 -0800

    Fixed the scenery and added a few more enemies.

--- Assets/Scripts/Entities/MapObject.cs
+++ Assets/Scripts/Entities/MapObject.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapObject : MonoBehaviour {
-    public int tile;                // The tile that the object is placed on
-    public Vector3 spriteOffset;    // The offest of the sprite compared to the tile
+    public int tile;                        // The tile that the object is placed on
+    public Vector3 spriteOffset;            // The offest of the sprite compared to the tile
+    private SpriteRenderer _spriteRenderer; // The Sprite Renderer component attached to child
 
 	// Use this for initialization
 	void Start ()
     {
         transform.Find("Sprite").localPosition = spriteOffset;
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -18,8 +20,10 @@ public class MapObject : MonoBehaviour {
         // Place the object
         if (GameController.map != null)
         {
-            transform.position = new Vector3(GameController.map.tiles[tile].transform.position.x, GameController.map.tiles[tile].transform.position.y, BehindPlayer() ? -2 : -0.5f);
+            transform.position = GameController.map.tiles[tile].transform.position;
         }
+        // Move in front of player if player is behind it
+        _spriteRenderer.sortingOrder = BehindPlayer() ? 200 : 50;
     }
 
     // Returns true if the player is behind the object
